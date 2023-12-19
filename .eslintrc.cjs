a57759de09fa72676885d0facbf141c438a8e018
fansoa <fverin.contactpro@gmail.com>
2023-12-20 00:13:10 +0100

    🔧: Add airbnb-base styleguide, bind Eslint to Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: { react: { version: '18.2' }},
+  plugins: [
+      'react-refresh',
+      'prettier'
+    ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
