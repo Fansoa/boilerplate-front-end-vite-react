258d92654807ade5c72b061e48eb5ca1ef14479d
fansoa <fverin.contactpro@gmail.com>
2023-12-22 02:59:54 +0100

    ✅(TestComponent): Add unit test

---
+++ src/components/TestComponent/test/TestComponent.test.jsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, expect } from "vitest";
+import TestComponent from "../index.jsx";
+import renderWithIntegrationProvider from "../../../../configs/vitest/CustomRender.jsx";
+
+describe("TestComponent", () => {
+  let container;
+
+  beforeEach(() => {
+    container = renderWithIntegrationProvider(<TestComponent />).container;
+  });
+
+  it("Label is displayed", () => {
+    const label = screen.getByText(/testcomponent/i);
+    expect(label).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("isClicked is displayed when button is clicked", async () => {
+    const button = screen.getByRole("button", { name: /bouton/i });
+    await userEvent.click(button);
+    const isClicked = screen.getByText(/isclicked/i);
+    expect(isClicked).toBeInTheDocument();
+    await userEvent.click(button);
+    expect(isClicked).not.toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
