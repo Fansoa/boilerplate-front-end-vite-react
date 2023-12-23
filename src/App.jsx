fdcb9b1a8d73e6bbb0cf8577ad310ea0fea9f77a
fansoa <fverin.contactpro@gmail.com>
2023-12-23 23:11:03 +0100

    ✨(App): Implement Routes in BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "@emotion/react";
 import "./normalize.css";
 
-import TestComponent from "./components/TestComponent/index.jsx";
+import { BrowserRouter } from "react-router-dom";
 import theme from "./theme";
+
+import Routes from "./routes/index.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <TestComponent />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
