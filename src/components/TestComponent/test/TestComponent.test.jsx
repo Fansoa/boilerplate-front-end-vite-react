e9c8819c2cd87438d9e775c3bd3d3a22c7339b36
fansoa <fverin.contactpro@gmail.com>
2024-04-13 23:31:46 +0200

    ✅: update TestComponent test

--- src/components/TestComponent/test/TestComponent.test.jsx
+++ src/components/TestComponent/test/TestComponent.test.jsx
@@ -1,13 +1,28 @@
 import { screen } from "@testing-library/react";
 import TestComponent from "@/src/components/TestComponent";
 import renderWithIntegrationProvider from "@/configs/vitest/customRender";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 
-it("Label is displayed", () => {
-  const result = renderWithIntegrationProvider(
-    <TestComponent label="label" />,
-  ).container;
-
-  const label = screen.getByText("label");
-  expect(label).toBeInTheDocument();
-  expect(result).toMatchSnapshot();
+describe("TestComponent", async () => {
+  it("Label is displayed", async () => {
+    const { container } = renderWithIntegrationProvider(
+      <TestComponent label="label" />,
+    );
+    const label = screen.getByText("label");
+    expect(label).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+  it("Button has been clicked", async () => {
+    const { container } = renderWithIntegrationProvider(
+      <TestComponent label="label" />,
+    );
+    const user = userEvent.setup();
+    await act(async () => {
+      await user.click(screen.getByRole("button", { name: /label/i }));
+    });
+    const toastMessage = screen.getByText(/Button has been clicked/i);
+    expect(toastMessage).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
 });
