a5f6b7e74f94d0d23dad2829ede9290af1b451a8
fansoa <fverin.contactpro@gmail.com>
2024-04-17 01:34:53 +0200

    ✨: add TestForm

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { useRoutes } from "react-router-dom";
 import TestComponent from "@/src/components/TestComponent";
+import TestForm from "@/src/components/TestForm";
 
 const Routes = () => {
   const routes = useRoutes([
     {
       path: "/",
       element: <TestComponent />,
+    },
+    {
+      path: "/react-hook-form",
+      element: <TestForm />,
     },
   ]);
   return routes;
